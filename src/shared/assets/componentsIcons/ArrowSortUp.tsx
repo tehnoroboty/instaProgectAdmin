a2c9075e2f3701dca48f90459bb689f583bd1e36
dnemirich <dnemirich@gmail.com>
2025-05-27 16:26:03 +0200

    SCRUM-122 fix: add arrows components

---
+++ src/shared/assets/componentsIcons/ArrowSortUp.tsx
@@ -0,0 +1,10 @@
+import { Ref, type SVGProps, forwardRef, memo } from 'react'
+
+const SvgArrowSortUp = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={8} height={6} fill="none" {...props} ref={ref}><path fill="currentColor" d="m4 0 3.464 4.5H.536z" />
+    </svg>
+);
+const ForwardRef = forwardRef(SvgArrowSortUp)
+const Memo = memo(ForwardRef)
+
+export default Memo
