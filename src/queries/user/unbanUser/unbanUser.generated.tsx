3eb38fab8cf715858e84501a7bbfe9a6a111be0f
Timofeev Yury <yurik-007@mail.ru>
2025-06-10 19:37:12 +0300

    SCRUM-126 feat: add function and graphQL

--- src/queries/user/unbanUser/unbanUser.generated.tsx
+++ src/queries/user/unbanUser/unbanUser.generated.tsx
@@ -1,24 +1,22 @@
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
+import * as Types from '../../types';
 
-import * as Types from '../../types'
-const defaultOptions = {} as const
-
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
 export type UnbanUserMutationVariables = Types.Exact<{
-  id: Types.Scalars['Int']['input']
-}>
+  id: Types.Scalars['Int']['input'];
+}>;
 
-export type UnbanUserMutation = { __typename?: 'Mutation'; unbanUser: boolean }
+
+export type UnbanUserMutation = { __typename?: 'Mutation', unbanUser: boolean };
+
 
 export const UnbanUserDocument = gql`
-  mutation UnbanUser($id: Int!) {
-    unbanUser(userId: $id)
-  }
-`
-export type UnbanUserMutationFn = Apollo.MutationFunction<
-  UnbanUserMutation,
-  UnbanUserMutationVariables
->
+    mutation UnbanUser($id: Int!) {
+  unbanUser(userId: $id)
+}
+    `;
+export type UnbanUserMutationFn = Apollo.MutationFunction<UnbanUserMutation, UnbanUserMutationVariables>;
 
 /**
  * __useUnbanUserMutation__
@@ -37,19 +35,10 @@ export type UnbanUserMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useUnbanUserMutation(
-  baseOptions?: Apollo.MutationHookOptions<UnbanUserMutation, UnbanUserMutationVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useMutation<UnbanUserMutation, UnbanUserMutationVariables>(
-    UnbanUserDocument,
-    options
-  )
-}
-export type UnbanUserMutationHookResult = ReturnType<typeof useUnbanUserMutation>
-export type UnbanUserMutationResult = Apollo.MutationResult<UnbanUserMutation>
-export type UnbanUserMutationOptions = Apollo.BaseMutationOptions<
-  UnbanUserMutation,
-  UnbanUserMutationVariables
->
+export function useUnbanUserMutation(baseOptions?: Apollo.MutationHookOptions<UnbanUserMutation, UnbanUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<UnbanUserMutation, UnbanUserMutationVariables>(UnbanUserDocument, options);
+      }
+export type UnbanUserMutationHookResult = ReturnType<typeof useUnbanUserMutation>;
+export type UnbanUserMutationResult = Apollo.MutationResult<UnbanUserMutation>;
+export type UnbanUserMutationOptions = Apollo.BaseMutationOptions<UnbanUserMutation, UnbanUserMutationVariables>;
---
+++ src/shared/lib/constants/select.ts
@@ -0,0 +1,31 @@
+import { UserBlockStatus } from '@/src/queries/types'
+import { BanReason } from '@/src/shared/types/types'
+
+export const SHOW_USERS_PAGE_SIZE_OPTIONS = [
+  { value: '8', valueTitle: '8' },
+  { value: '10', valueTitle: '10' },
+  { value: '20', valueTitle: '20' },
+  { value: '30', valueTitle: '30' },
+  { value: '50', valueTitle: '50' },
+  { value: '100', valueTitle: '100' },
+]
+
+export const SELECT_OPTIONS = [
+  { value: UserBlockStatus.All, valueTitle: 'Not selected' },
+  { value: UserBlockStatus.Blocked, valueTitle: 'Blocked' },
+  { value: UserBlockStatus.Unblocked, valueTitle: 'Not blocked' },
+]
+
+export const SELECT_REASON = [
+  { value: 'Bad behavior', valueTitle: 'Bad behavior' },
+  {
+    value: 'Advertising placement',
+    valueTitle: 'Advertising placement',
+  },
+  {
+    value: 'Another reason',
+    valueTitle: 'Another reason',
+  },
+]
+
+export const DEFAULT_BAN_REASON: BanReason = SELECT_REASON[2].value
